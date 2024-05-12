e6a0e5fcbc5f1c00891a24e6889e224882d1a291
Anandakus <najwaakusnadi2@gmail.com>
2024-05-12 10:36:33 +0700

    Fix detailabsensi.php: API url

--- detailabsensi.php
+++ detailabsensi.php
@@ -30,7 +30,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Absensi | UNINDRA</title>
+  <title>Absensi | UNMER MALANG</title>
 
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -54,7 +54,7 @@
         <div class="sidebar-brand-icon">
           <i class="fas fa-university"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">UNINDRA</div>
+        <div class="sidebar-brand-text mx-3">UNMER MALANG</div>
       </a>
 
       <!-- Divider -->
--- detaileditabsensi.php
+++ detaileditabsensi.php
@@ -27,7 +27,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Edit Absensi | UNINDRA</title>
+  <title>Edit Absensi | UNMER MALANG</title>
 
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -51,7 +51,7 @@
         <div class="sidebar-brand-icon">
           <i class="fas fa-university"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">UNINDRA</div>
+        <div class="sidebar-brand-text mx-3">UNMER MALANG</div>
       </a>
 
       <!-- Divider -->
@@ -186,7 +186,7 @@
                   <table class="table table-bordered" id="" width="100%" cellspacing="0">
                     <thead>
                         <tr align="center">
-                        <th>NPM</th>
+                        <th>NIM</th>
                         <th>Nama</th>
                         <th>Kelas</th>
                         </tr>
